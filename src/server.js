568d42f755bc24da4a8b3111d30e11ad0b613514
Deniz Akcal <denizakcal@mail.com>
2019-10-09 00:48:50 -0400

    I got two different (test) (Postgresql) tables to be accessible separately.

--- src/server.js
+++ src/server.js
@@ -8,12 +8,77 @@ app.use(bodyParser.json());
 app.get('/',function(req,res){
   res.sendFile("/home/deniz/Dropbox/SOEN341/SOEN341_Project/SOEN341_Project/src/index.html");
 });
+
+function getSqlResultOneServer(callback) {
+  const pg = require('pg');
+  const pool = new pg.Pool({
+  user: 'deniz',
+  host: '127.0.0.1',
+  database: 'soen341db',
+  password: 'cncrd',
+  port: '5432'});
+
+  //pool.query("SELECT NOW()", (err, res) => {
+    pool.query("SELECT * FROM masa", (theError, theResult) => {
+      console.log(theError, theResult);
+      
+      //console.log(theError,theResult['rows']);
+      //console.log(theError,theResult['rows'][0]);
+      var dataToReturn = theResult['rows'][0]['n1'];
+      console.log("dataToReturn: " + dataToReturn); // This works properly.
+      pool.end();
+      // also need to handle if there are errors, this assumes success
+      callback(dataToReturn)      
+  });
+
+  //return dataToReturn;
+}
+//module.exports.getSqlResult = getSqlResultOne;
+
+function getSqlResultTwoServer(callback) {
+  const pg = require('pg');
+  const pool = new pg.Pool({
+  user: 'deniz',
+  host: '127.0.0.1',
+  database: 'soen341db',
+  password: 'cncrd',
+  port: '5432'});
+
+    pool.query("SELECT * FROM sometbl", (theError, theResult) => {
+      //console.log(theError, theResult);
+      
+      //console.log(theError,theResult['rows']);
+      //console.log(theError,theResult['rows'][0]);
+      var dataToReturn = theResult['rows'][0]['n'];
+      console.log("dataToReturn: " + dataToReturn); // This works properly.
+      pool.end();
+      // also need to handle if there are errors, this assumes success
+      callback(dataToReturn)      
+  });
+}
+
 app.post('/login',function(req,res){
-  var user_name=req.body.username;
-  var password=req.body.password;
-  console.log("User name = "+user_name+", password is "+password);
-  res.end("yes");
+  //console.log("getSqlResult(): " + getSqlResult()); // Why is this returning undefined?
+  //var user_name=req.body.username;
+  //var password=req.body.password;
+  //console.log("User name = "+user_name+", password is "+password);
+  
+  getSqlResultOneServer((dataFromCallback) => {
+    //this code is the actual callback
+    //console.log('yo', dataFromCallback)
+    res.end(`${dataFromCallback}`);
+  })
 });
+
+app.post('/db2',function(req,res){
+  getSqlResultTwoServer((dataFromCallback) => {
+    //this code is the actual callback
+    //console.log('yo', dataFromCallback)
+    res.end(`${dataFromCallback}`);
+  })
+  //res.end('db2_yo');
+});
+
 app.listen(3000,function(){
   console.log("Started on PORT 3000");
-})
+});
