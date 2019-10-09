d59920b3aa3289faaf2efc58592a4b15ecd84ce2
Deniz Akcal <denizakcal@mail.com>
2019-10-09 18:21:31 -0400

    I improved the database tables initializer.

--- src/create_tables.sql
+++ src/create_tables.sql
@@ -1,5 +1,11 @@
-CREATE TABLE IF NOT EXISTS Channels(channelId SERIAL /*INT*/, messageContent TEXT, messageNumber INT, messageTimestamp TIMESTAMP, userIdOfMessage INT, PRIMARY KEY(channelId, messageNumber));
+-- CREATE TABLE IF NOT EXISTS Channels(channelId SERIAL /*INT*/, messageContent TEXT, messageNumber INT, messageTimestamp TIMESTAMP, userIdOfMessage INT, PRIMARY KEY(channelId, messageNumber));
 
-CREATE TABLE IF NOT EXISTS Users(userId SERIAL /*INT*/, firstName TEXT, lastName TEXT, PRIMARY KEY(userId));
+-- CREATE TABLE IF NOT EXISTS Users(userId SERIAL /*INT*/, firstName TEXT, lastName TEXT, PRIMARY KEY(userId));
 
-CREATE TABLE IF NOT EXISTS userChannelCombinations(channelId INT, userId INT, PRIMARY KEY(channelId, userId));
+-- CREATE TABLE IF NOT EXISTS userChannelCombinations(channelId INT, userId INT, PRIMARY KEY(channelId, userId));
+
+CREATE TABLE IF NOT EXISTS Users(userId SERIAL /*INT*/, userAccountName TEXT, firstName TEXT, lastName TEXT, PRIMARY KEY(userId));
+
+CREATE TABLE IF NOT EXISTS Messages(messageContent TEXT, timestamp TIMESTAMP, messageNumberInChannel INT, userIdOfAuthorOfThatMessage INT, channelId INT, PRIMARY KEY(messageNumberInChannel, userIdOfAuthorOfThatMessage, channelId));
+
+CREATE TABLE IF NOT EXISTS Channels(channelId SERIAL /*INT*/, channelName TEXT, PRIMARY KEY(channelId));
