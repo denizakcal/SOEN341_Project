63d2fad526711b8b3a406da7b6cf59ecf8c69acb
KennanHoa <kennan.hoa@mechasys.ca>
2019-10-09 21:54:39 -0400

    project folder

---
+++ SOEN341-CK/README.md
@@ -0,0 +1,16 @@
+# CSS Variables
+
+Quick start:
+
+```
+$ npm install
+$ npm run build
+````
+
+## Development
+
+Run Webpack in watch-mode to continually compile the JavaScript as you work:
+
+```
+$ npm run watch
+```
---
+++ SOEN341-CK/config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/bb4d357a-bfef-46f3-bddb-4b90bdb33a68/token";
+const instanceLocator = "v1:us1:bb4d357a-bfef-46f3-bddb-4b90bdb33a68";
+
+export { tokenUrl, instanceLocator }
