4421a466fc67eb0e4c9d1e469d40c61928e536c5
Deniz Akcal <denizakcal@mail.com>
2019-10-09 23:56:15 -0400

    I made a minor improvement in create_tables.sql file.

--- src/create_tables.sql
+++ src/create_tables.sql
@@ -12,6 +12,8 @@ DROP TABLE IF EXISTS Channels;
 
 CREATE TABLE IF NOT EXISTS Users(userId SERIAL /*INT*/, userAccountName TEXT, userAccountPassword TEXT, firstName TEXT, lastName TEXT, PRIMARY KEY(userId)); -- Make password storing better later.
 
-CREATE TABLE IF NOT EXISTS Messages(messageContent TEXT, timestamp TIMESTAMP, messageNumberInChannel INT, userIdOfAuthorOfThatMessage INT, channelId INT, PRIMARY KEY(messageNumberInChannel, userIdOfAuthorOfThatMessage, channelId));
-
 CREATE TABLE IF NOT EXISTS Channels(channelId SERIAL /*INT*/, channelName TEXT, PRIMARY KEY(channelId));
+
+-- ADD THIS LATER!!!: CREATE TABLE IF NOT EXISTS UserChannelAssociations(userId INT, channelId INT, PRIMARY KEY(userId,channelId));
+
+CREATE TABLE IF NOT EXISTS Messages(messageContent TEXT, timestamp TIMESTAMP, messageNumberInChannel INT, userIdOfAuthorOfThatMessage INT, channelId INT, PRIMARY KEY(messageNumberInChannel, userIdOfAuthorOfThatMessage, channelId));
