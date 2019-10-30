fd52f780935b4e5822bcdc18ff9eedd0921d6ef0
Oussama <oussamus_84@hotmail.com>
2019-10-30 13:40:32 -0400

    From Kennan
    Update from Kennan

---
+++ SOEN341-CKv2/server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const Chatkit = require('@pusher/chatkit-server')
+
+const app = express()
+
+const chatkit = new Chatkit.default({
+  instanceLocator: 'v1:us1:bb4d357a-bfef-46f3-bddb-4b90bdb33a68',
+  key: '8d6d73ff-d70b-4378-9f80-96f7f02bddf9:weT6s0I+UA7PwX42OR5/XugeGvgd5Q1zb7a0g3ZmHto=',
+})
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/users', (req, res) => {
+  const { username } = req.body
+  chatkit
+    .createUser({
+      id: username,
+      name: username
+    })
+    .then(() => res.sendStatus(201))
+    .catch(error => {
+      if (error.error === 'services/chatkit/user_already_exists') {
+        res.sendStatus(200)
+      } else {
+        res.status(error.status).json(error)
+      }
+    })
+})
+
+app.post('/authenticate', (req, res) => {
+  const authData = chatkit.authenticate({ userId: req.query.user_id })
+  res.status(authData.status).send(authData.body)
+})
+
+const PORT = 3001
+app.listen(PORT, err => {
+  if (err) {
+    console.error(err)
+  } else {
+    console.log(`Running on port ${PORT}`)
+  }
+})
