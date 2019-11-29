dc618da828c093611e8af7ade6547e91972668aa
Deniz Akcal <denizakcal@mail.com>
2019-11-29 18:52:31 -0500

    I made it so that the situation where no room is chosen is tested to be working as expected.

--- src/components/MessageList.test.js
+++ src/components/MessageList.test.js
@@ -3,14 +3,29 @@ import {shallow} from 'enzyme'; // const {shallow} = require('enzyme');
 import MessageList from './MessageList'; //const Message = require('./Message');
 import { exportAllDeclaration } from '@babel/types';
 
+const setUp = (props={}) => {
+    const component = shallow(<MessageList {...props} />);
+    return component;
+};
+
 describe('MessageList component', () => {
-    it('It should also render without errors', () => {
+    it('should render one message-list div containing one join-room div (if there is no room chosen)', () => {
         
-        const component = shallow(<MessageList />)
-        console.log(component.debug())
+        const messageListRootComponentShallowWrapper = setUp(); //shallow(<MessageList />)
+        console.log('Logging component.debug():', messageListRootComponentShallowWrapper.debug())
+        expect(messageListRootComponentShallowWrapper.length).toBe(1);
         
-        const wrapper = component.find('.join-room')
-        expect(wrapper.length).toBe(1);
-        //expect(1).toBe(1)
+        const messageListDivShallowWrapper = messageListRootComponentShallowWrapper.childAt(0);
+        console.log('Logging messageListDiv.debug():', messageListDivShallowWrapper.debug());
+        const joinRoomDiv = messageListRootComponentShallowWrapper.find('.join-room')
+        expect(joinRoomDiv.length).toBe(1);
     });
+
+    // TODO:
+    // it('should render one message-list div containing one more message than previously (if a room is chosen)', () => {//TODO
+    //     // // If room is chosen
+    //     const component = setUp(0);
+    //     const wrapper = component.find('.message-list')
+    //     expect(wrapper.length).toBe(1);
+    // });
 });
