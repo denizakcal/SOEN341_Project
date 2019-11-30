4f62d162d008a3ef5dff2accc8445dfaa6ec8fd8
Deniz Akcal <denizakcal@mail.com>
2019-11-29 19:45:11 -0500

    I replaced the messageListRootComponentShallowWrapper, which along with the other test(s) would be a repetition, declarations with a (jest) beforeEach function.

--- src/components/MessageList.test.js
+++ src/components/MessageList.test.js
@@ -1,6 +1,7 @@
 import React from 'react'; // const React = require('react');
 import {shallow} from 'enzyme'; // const {shallow} = require('enzyme');
 import MessageList from './MessageList'; //const Message = require('./Message');
+import SendMessageForm from './SendMessageForm';
 import { exportAllDeclaration } from '@babel/types';
 
 const setUp = (props={}) => {
@@ -9,23 +10,33 @@ const setUp = (props={}) => {
 };
 
 describe('MessageList component', () => {
-    it('should render one message-list div containing one join-room div (if there is no room chosen)', () => {
+
+    let messageListRootComponentShallowWrapper;
+    beforeEach(() => {
+        messageListRootComponentShallowWrapper = setUp();
+    });
+
+    it('should render one message-list div containing one join-room div (AS IF THERE IS NO ROOM CHOSEN)', () => {
         
-        const messageListRootComponentShallowWrapper = setUp(); //shallow(<MessageList />)
+        //const messageListRootComponentShallowWrapper = setUp(); //shallow(<MessageList />)
         console.log('Logging component.debug():', messageListRootComponentShallowWrapper.debug())
         expect(messageListRootComponentShallowWrapper.length).toBe(1);
         
         const messageListDivShallowWrapper = messageListRootComponentShallowWrapper.childAt(0);
-        console.log('Logging messageListDiv.debug():', messageListDivShallowWrapper.debug());
+        console.log('Logging messageListDivShallowWrapper.debug():', messageListDivShallowWrapper.debug());
         const joinRoomDiv = messageListRootComponentShallowWrapper.find('.join-room')
         expect(joinRoomDiv.length).toBe(1);
     });
 
     // TODO:
-    // it('should render one message-list div containing one more message than previously (if a room is chosen)', () => {//TODO
-    //     // // If room is chosen
-    //     const component = setUp(0);
-    //     const wrapper = component.find('.message-list')
-    //     expect(wrapper.length).toBe(1);
+    // it('should render one message-list div containing one more message than previously (AS IF A ROOM IS CHOSEN)', () => {//TODO
+    //     //const messageListRootComponentShallowWrapper = setUp(); //shallow(<MessageList />)
+    //     const messageListDivShallowWrapper = messageListRootComponentShallowWrapper.find('.message-list')
+    //     const initialMessageListDivShallowWrapperAmount = messageListDivShallowWrapper.length;
+    //     console.log('Logging messageListDivShallowWrapper.debug() #1:', messageListDivShallowWrapper.debug());
+    //     //SendMessageForm smf = new SendMessageForm();
+    //     //smf.setState();
+    //     console.log('Logging messageListDivShallowWrapper.debug() #2:', messageListDivShallowWrapper.debug());
+    //     expect(messageListDivShallowWrapper.length > initialMessageListDivShallowWrapperAmount).toBeTruthy();
     // });
 });
